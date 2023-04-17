8618cb0f8b60bc15876054e79ddd38c6c8b75a12
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-17 09:51:46 +0200

    Separated setup mode from coming play mode

--- assemble.sh
+++ assemble.sh
@@ -2,10 +2,12 @@
 
 # Assemble directories for release/installation
 
+rm -rf output
+
 mkdir -p output/apps/lua/drift-mode
 mkdir -p output/lua/drift-mode
-mkdir -p output/extension/lua/new-modes/drift-mode
+mkdir -p output/extension/lua/new-modes
 
 cp app/* output/apps/lua/drift-mode
 cp lua_libs/* output/lua/drift-mode
-cp mode/* output/extension/lua/new-modes/drift-mode
+cp -r modes/* output/extension/lua/new-modes
