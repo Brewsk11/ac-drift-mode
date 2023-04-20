32634f1a35898ccc9f93a27858d7355821c904ed
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-20 23:03:14 +0200

    New score visualization graphic

--- modes/drift-mode/mode.lua
+++ modes/drift-mode/mode.lua
@@ -391,21 +391,95 @@ function script.draw3D()
   end
 end
 
+function drawModifiers()
+  -- local mult_speed = cursor_data.dspeed
+  -- local mult_angle = cursor_data.dangle
+  -- local current_ratio = cursor_data.dratio
+
+  local speedRect = {
+    origin = vec2(0, 0),
+    size = vec2(300, 25),
+    min = min_speed,
+    max = max_speed,
+    name = "Speed",
+    color = rgbm(230 / 255, 138 / 255, 46 / 255, 1)
+  }
+
+  local angleRect = {
+    origin = vec2(0, 30),
+    size = vec2(300, 25),
+    min = min_angle,
+    max = max_angle,
+    name = "Angle",
+    color = rgbm(20 / 255, 204 / 255, 112 / 255, 1)
+  }
+
+  local ratioRect = {
+    origin = vec2(0, 60),
+    size = vec2(300, 25),
+    min = 0,
+    max = 100,
+    name = "Zone depth",
+    color = rgbm(112 / 255, 20 / 255, 204 / 255, 1)
+  }
+
+  local compoundRect = {
+    origin = vec2(0, 100),
+    size = vec2(300, 30),
+    min = 0,
+    max = 100,
+    name = "Score multiplier",
+    color = rgbm(180 / 255, 180 / 255, 180 / 255, 1)
+  }
+
+  local windowWidth = ui.windowWidth()
+  local widgetSize = vec2(300, 130)
+  local widgetOrigin = vec2(windowWidth / 2 - widgetSize.x / 2, 200)
+
+  local function drawInfobar(rect, value)
+    if value == nil then return end
+    ui.drawRect(rect.origin, rect.origin +  rect.size, rect.color, 2)
+
+    if value ~= nil then
+      ui.drawRectFilled(rect.origin, rect.origin +  vec2(rect.size.x * value, rect.size.y), rect.color, 2)
+      ui.setCursor(rect.origin)
+      ui.dwriteTextAligned(rect.name, 10, ui.Alignment.Start, ui.Alignment.End, rect.size)
+      ui.setCursor(rect.origin)
+      ui.dwriteTextAligned(string.format("%.0f%%", value * 100), 18, ui.Alignment.Center, ui.Alignment.Center, rect.size)
+    end
+
+    ui.setCursor(rect.origin)
+    ui.dwriteTextAligned(rect.min, 10, ui.Alignment.Start, ui.Alignment.Start, rect.size)
+    ui.setCursor(rect.origin)
+    ui.dwriteTextAligned(rect.max, 10, ui.Alignment.End, ui.Alignment.End, rect.size)
+  end
+
+  local ratio_nil = current_ratio
+  if current_ratio == nil then ratio_nil = 0 end
+
+  ui.beginTransparentWindow('modifiers', widgetOrigin, widgetSize)
+  ui.pushDWriteFont("ACRoboto700.ttf")
+  drawInfobar(speedRect, mult_speed)
+  drawInfobar(angleRect, mult_angle)
+  drawInfobar(ratioRect, ratio_nil)
+  if mult_angle and mult_speed then
+    drawInfobar(compoundRect, mult_speed * mult_angle * ratio_nil)
+  end
+  ui.endTransparentWindow()
+end
+
 function script.drawUI()
   local br = vec2(0, 60)
   local ibr = vec2(0, 15)
   local orig = vec2(0, 0)
 
+  drawModifiers()
+
+  ui.beginTransparentWindow('modifiers', vec2(500, 100), vec2(600, 300))
+  ui.endTransparentWindow()
+
   ui.beginTransparentWindow('driverStopwatch', vec2(150, 100), vec2(600, 300))
   ui.pushFont(ui.Font.Huge)
   ui.drawText(string.format("Total score: %.0f", total_score), orig, rgbm(1, 1, 1, 1))
-  ui.pushFont(ui.Font.Italic)
-  ui.drawText(string.format('Angle multiplier: %.2f', mult_angle), orig:add(br), rgbm(1, 1, 1, 1))
-  ui.drawText(string.format('Speed multiplier: %.2f', mult_speed), orig:add(ibr), rgbm(1, 1, 1, 1))
-  if current_ratio ~= nil then
-    ui.drawText(string.format('Zone ratio: %.1f', current_ratio), orig:add(ibr), rgbm(1, 3, 1, 1))
-    ui.pushFont(ui.Font.Title)
-    ui.drawText(string.format('Currently scoring: %.2f', current_scoring), orig:add(ibr), rgbm(1, 3, 1, 1))
-  end
   ui.endTransparentWindow()
 end
