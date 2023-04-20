048e15c98a02e10c6982aaf4f9b32dc02adeb86e
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-20 22:59:59 +0200

    Autofill manfiest versions
    Basing on commit id or tag

--- assemble.sh
+++ assemble.sh
@@ -2,6 +2,12 @@
 
 # Assemble directories for release/installation
 
+VERSION=$(git describe --tags)
+if [[ -z  "$VERSION" ]]; then
+    VERSION=$(git rev-parse HEAD)
+    VERSION=${VERSION:0:7}
+fi
+
 rm -rf output
 
 mkdir -p output/ac_gamedir/apps/lua/drift-mode
@@ -15,3 +21,5 @@ cp -r modes/* output/ac_gamedir/extension/lua/new-modes
 cp config/*   output/ac_usercfg/cfg/extension/drift-mode
 
 cp ./INSTALL.md output/
+
+find ./output -name *.ini -exec sed -i "s/VERSION = XXX/VERSION = $VERSION/"  {} \;
