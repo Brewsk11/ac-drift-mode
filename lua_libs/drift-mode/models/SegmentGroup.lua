fcd70f9a6968698c98875415fd1c5fb7f2a2babf
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-25 19:17:33 +0200

    SegmentGroup - `pop()`, `continuous()`, `closed()`

--- lua_libs/drift-mode/models/SegmentGroup.lua
+++ lua_libs/drift-mode/models/SegmentGroup.lua
@@ -1,3 +1,4 @@
+local Assert = require('drift-mode/assert')
 local Segment = require('drift-mode/models/Segment')
 
 ---@class SegmentGroup Ordered group of segments
@@ -29,7 +30,38 @@ function SegmentGroup.append(self, segment)
     self.segments[#self.segments+1] = segment
 end
 
-local Assert = require('drift-mode/assert')
+---Pop the last segment from the group
+---@param self SegmentGroup
+---@return Segment
+function SegmentGroup.pop(self)
+    local segment = self.segments[self:count()]
+    self.segments[self:count()] = nil
+    return segment
+end
+
+---Is every segment's tail at the same point as next's segment head
+---@param self SegmentGroup
+---@return boolean
+function SegmentGroup.continuous(self)
+    Assert.MoreThan(self:count(), 0, "Cannot calculate continous on empty set")
+    for idx = 1, self:count() - 1 do
+        if self.segments[idx].tail:get() ~= self.segments[idx+1].head:get() then
+            return false
+        end
+    end
+    return true
+end
+
+---Is the group continous and is last segment connected to the first
+---@param self SegmentGroup
+---@return boolean
+function SegmentGroup.closed(self)
+    Assert.MoreThan(self:count(), 0, "Cannot calculate closed on empty set")
+    return
+        self:continuous() and
+        self.segments[self:count()].tail:get() == self.segments[1].head:get()
+end
+
 local function test()
     local points = {
         Point.new("a", vec3(1, 1, 1)),
@@ -57,6 +89,25 @@ local function test()
     -- SegmentGroup:append()
     grp:append(Segment.new(points[3], points[4]))
     Assert.Equal(grp:count(), 3, "Incorrect segment count after appending a segment")
+
+    -- SegmentGroup:continous()
+    Assert.Equal(grp:continuous(), true, "The segment group does not seem to be continous, whereas it should be")
+    local seg_test_a = Segment.new(points[3], points[1])
+    grp:append(seg_test_a)
+    Assert.Equal(grp:continuous(), false, "The segment group does seem to be continous, whereas it should not be")
+
+    -- SegmentGroup:pop()
+    local seg_test_b = grp:pop()
+    local a_head, a_tail = seg_test_a:get()
+    local b_head, b_tail = seg_test_b:get()
+    Assert.Equal(a_head, b_head, "The popped segment is incorrect")
+    Assert.Equal(a_tail, b_tail, "The popped segment is incorrect")
+
+    -- Segment:closed()
+    grp:append(Segment.new(points[4], points[1]))
+    Assert.Equal(grp:closed(), true, "The segment group does not seem to be closed, whereas it should be")
+    grp:append(Segment.new(points[1], points[2]))
+    Assert.Equal(grp:closed(), false, "The segment group does seem to be closed, whereas it should not be")
 end
 test()
 
