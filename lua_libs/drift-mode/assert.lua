7e28f9268bcbf94b359031cde871290bed343167
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-25 21:42:28 +0200

    Fix for asserts

--- lua_libs/drift-mode/assert.lua
+++ lua_libs/drift-mode/assert.lua
@@ -22,19 +22,19 @@ function Assert.NotEqual(a, b, message)
 end
 
 function Assert.LessThan(a, b, message)
-    _assert(a, b, function (_a, _b) return _a > _b end, "Value not less than constant", message)
+    _assert(a, b, function (_a, _b) return _a < _b end, "Value not less than constant", message)
 end
 
 function Assert.MoreThan(a, b, message)
-    _assert(a, b, function (_a, _b) return _a < _b end, "Value not greater than constant", message)
+    _assert(a, b, function (_a, _b) return _a > _b end, "Value not greater than constant", message)
 end
 
 function Assert.LessOrEqual(a, b, message)
-    _assert(a, b, function (_a, _b) return _a >= _b end, "Value greater than constant", message)
+    _assert(a, b, function (_a, _b) return _a <= _b end, "Value greater than constant", message)
 end
 
 function Assert.MoreOrEqual(a, b, message)
-    _assert(a, b, function (_a, _b) return _a <= _b end, "Value less than constant", message)
+    _assert(a, b, function (_a, _b) return _a >= _b end, "Value less than constant", message)
 end
 
 return Assert
