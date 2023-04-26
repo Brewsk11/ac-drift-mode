bc66143b73c01f2ae5253b6710b12acd62b88867
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-26 23:01:22 +0200

    `ClippingPoint` model

---
+++ lua_libs/drift-mode/models/ClippingPoint.lua
@@ -0,0 +1,70 @@
+local Assert = require('drift-mode/assert')
+local S = require('drift-mode/serializer')
+
+---@class ClippingPoint Class representing a drift scoring zone
+---@field name string Name of the zone
+---@field origin Point
+---@field direction vec3
+---@field length number
+---@field maxPoints integer Maximum points possible to score in the zone (in a perfect run)
+local ClippingPoint = {}
+ClippingPoint.__index = ClippingPoint
+
+local color_origin = rgbm(3, 0, 0, 1)
+local color_pole = rgbm(0, 0, 3, 0.4)
+local color_arrow = rgbm(0, 0, 3, 1)
+
+function ClippingPoint.serialize(self)
+    local data = {
+        __class = "ClippingPoint",
+        name = S.serialize(self.name),
+        origin = self.origin:serialize(),
+        direction = S.serialize(self.direction),
+        length = S.serialize(self.length),
+        maxPoints = S.serialize(self.maxPoints)
+    }
+    return data
+end
+
+function ClippingPoint.deserialize(data)
+    Assert.Equal(data.__class, "ClippingPoint", "Tried to deserialize wrong class")
+
+    local obj = ClippingPoint.new(
+        S.deserialize(data.name),
+        Point.deserialize(data.origin),
+        S.deserialize(data.direction),
+        S.deserialize(data.length),
+        S.deserialize(data.maxPoints)
+    )
+    return obj
+end
+
+---@param name string
+---@param origin Point
+---@param direction vec3
+---@param length number
+---@param maxPoints integer
+---@return ClippingPoint
+function ClippingPoint.new(name, origin, direction, length, maxPoints)
+    local self = setmetatable({}, ClippingPoint)
+    self.name = name
+    self.origin = origin
+    self.direction = direction
+    self.length = length
+    self.maxPoints = maxPoints
+    return self
+end
+
+function ClippingPoint.draw(self)
+    self.origin:draw(0.6, color_origin)
+    render.debugArrow(self.origin:get(), self.origin:get() + self.direction * self.length, 0.1, color_arrow)
+    render.debugLine(self.origin:get(), self.origin:get() + vec3(0, 2, 0), color_pole)
+    render.debugText(self.origin:get() + vec3(0, 2, 0), self.name)
+end
+
+local Assert = require('drift-mode/assert')
+local function test()
+end
+test()
+
+return ClippingPoint
--- lua_libs/drift-mode/models/TrackConfig.lua
+++ lua_libs/drift-mode/models/TrackConfig.lua
@@ -3,19 +3,25 @@ local Zone = require('drift-mode/models/Zone')
 
 -- Track configration data
 
----@class TrackConfig Data class describing key car points positions for scoring purposes
----@field zones Zone[] Offset from car origin to the front bumper
+---@class TrackConfig
+---@field zones Zone[]
+---@field clippingPoints ClippingPoint[]
 local TrackConfig = {}
 TrackConfig.__index = TrackConfig
 
 function TrackConfig.serialize(self)
     local data = {
         __class = "TrackConfig",
-        zones = {}
+        zones = {},
+        clippingPoints = {}
     }
 
     for idx, zone in ipairs(self.zones) do
         data.zones[idx] = zone:serialize()
+    end
+
+    for idx, clipPoint in ipairs(self.clippingPoints) do
+        data.clippingPoints[idx] = clipPoint:serialize()
     end
 
     return data
@@ -31,15 +37,32 @@ function TrackConfig.deserialize(data)
         zones[idx] = Zone.deserialize(zone)
     end
 
+    local clippingPoints = {}
+    for idx, clipPoint in ipairs(data.clippingPoints) do
+        clippingPoints[idx] = ClippingPoint.deserialize(clipPoint)
+    end
+
     obj.zones = zones
+    obj.clippingPoints = clippingPoints
     return obj
 end
 
-function TrackConfig.new(zones)
+function TrackConfig.new(zones, clippingPoints)
     local self = setmetatable({}, TrackConfig)
     local _zones = zones or {}
     self.zones = _zones
+    local _clippingPoints = clippingPoints or {}
+    self.clippingPoints = _clippingPoints
     return self
+end
+
+function TrackConfig.draw(self)
+    for _, zone in ipairs(self.zones) do
+      zone:draw()
+    end
+    for _, clipPoint in ipairs(self.clippingPoints) do
+      clipPoint:draw()
+    end
 end
 
 local function test()
