b44202463f69cdc3dac71ed8ab9efbb31a5e08f7
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-04-27 20:40:00 +0200

    `ConfigIO` - specializing for track configs
    Due for a refactor after crunching current refactor

--- lua_libs/drift-mode/configio.lua
+++ lua_libs/drift-mode/configio.lua
@@ -3,6 +3,40 @@ local json = require('drift-mode/json')
 
 local ConfigIO = {}
 
+local usr_cfg_path = ac.getFolder(ac.FolderID.ExtCfgUser)  .. "\\drift-mode"
+local sys_cfg_path = ac.getFolder(ac.FolderID.ExtCfgSys) .. "\\drift-mode"
+
+local track_id = ac.getTrackID()
+
+local usr_track_config_dir = usr_cfg_path .. "\\tracks\\" .. track_id
+local sys_track_config_dir = sys_cfg_path .. "\\tracks\\" .. track_id
+
+function ConfigIO.listTrackConfigs()
+    local usr_configs = io.scanDir(usr_cfg_path .. "\\tracks\\" .. track_id)
+    local sys_configs = io.scanDir(sys_cfg_path .. "\\tracks\\" .. track_id)
+    local track_configs = {
+        user_configs = {},
+        official_configs = {}
+    }
+    for _, cfg_name in ipairs(usr_configs) do track_configs.user_configs[#track_configs.user_configs+1] = cfg_name end
+    for _, cfg_name in ipairs(sys_configs) do track_configs.official_configs[#track_configs.official_configs+1] = cfg_name end
+    return track_configs
+end
+
+---@param track_config TrackConfig
+function ConfigIO.saveTrackConfig(track_config)
+    io.createDir(usr_track_config_dir)
+    ConfigIO.saveConfig(usr_track_config_dir .. "\\" .. track_config.name, track_config)
+end
+
+function ConfigIO.loadTrackConfig(name, dir)
+    if dir == "official" then
+        return ConfigIO.loadConfig(sys_track_config_dir .. "\\" .. name)
+    else
+        return ConfigIO.loadConfig(usr_track_config_dir .. "\\" .. name)
+    end
+end
+
 function ConfigIO.loadConfig(path)
     assert(type(path) == 'string', 'Parameter "fileName" must be a string.')
     if not io.exists(path) then
@@ -13,17 +47,14 @@ function ConfigIO.loadConfig(path)
 	local file_content = file:read("a")
     file:close()
 
-    ac.debug("file_content", file_content)
     local json_repr = json.decode(file_content)
     local deserialized = Serializer.deserialize(json_repr)
 
-    ac.debug("json_repr", deserialized)
 	return deserialized
 end
 
 function ConfigIO.saveConfig(path, data)
     assert(type(path) == 'string', 'Parameter "fileName" must be a string.')
-	assert(type(data) == 'table', 'Parameter "data" must be a table.')
 	local file = assert(io.open(path, 'w+b'), 'Error loading file :' .. path)
 
     local serialized = Serializer.serialize(data)
@@ -31,16 +62,6 @@ function ConfigIO.saveConfig(path, data)
 
 	file:write(json_content)
 	file:close()
-end
-
-function ConfigIO.updateConfig(path, new_data)
-	local data = ConfigIO.loadConfig(path)
-
-    for k, v in pairs(new_data) do
-        data[k] = v
-    end
-
-    ConfigIO.saveConfig(path, data)
 end
 
 return ConfigIO
