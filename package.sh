c075cd051d91ff50a85bef68b46338b07ca4b601
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-05-01 00:08:33 +0200

    Updated packaging script

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+./assemble.sh
+
 VERSION=$(git describe --tags)
 if [[ -z  "$VERSION" ]]; then
     VERSION=$(git rev-parse HEAD)
@@ -10,5 +12,6 @@ fi
 echo "Packaging $VERSION"
 
 cd output
+rm -f ../packaged/ac-drift-mode-$VERSION.zip
 zip ../packaged/ac-drift-mode-$VERSION.zip -r ./*
 cd - 2>&1 > /dev/null
