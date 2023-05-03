cefb5b2d988e162f0da293a684fc776f4d76982b
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-05-03 19:36:25 +0200

    Add `Zone.isSegmentInZone()` with tests

--- lua_libs/drift-mode/models/Zone.lua
+++ lua_libs/drift-mode/models/Zone.lua
@@ -127,6 +127,47 @@ function Zone.isInZone(self, point, custom_origin)
     if hit_no % 2 == 1 then return true else return false end
 end
 
+---Check if a segment is inside the zone. Relatively expensive to compute.
+---
+---Naively assumes there's only one intersection. If both segment end-points are either in
+---or out it assumes the whole segment is in or out.
+---@param self Zone
+---@param segment Segment
+---@param custom_origin Point? Custom origin point, to check corretly it must be outside the zone
+---@return number fraction Fraction of the segment inside the zone: `1.0` fully inside, `0.0` fully outside
+function Zone.isSegmentInZone(self, segment, custom_origin)
+    local is_head_in_zone = self:isInZone(segment.head, custom_origin)
+    local is_tail_in_zone = self:isInZone(segment.tail, custom_origin)
+
+    ac.debug("head_in", is_head_in_zone)
+    ac.debug("tail_in", is_tail_in_zone)
+
+    if not is_head_in_zone and not is_tail_in_zone then return 0.0 end
+    if     is_head_in_zone and     is_tail_in_zone then return 1.0 end
+
+    -- Find which zone part is crossing the segment
+    for _, zone_segment in self:getPolygon():segment(true):iter() do
+        local hit = vec2.intersect(
+            segment.head:flat(),
+            segment.tail:flat(),
+            zone_segment.head:flat(),
+            zone_segment.tail:flat()
+        )
+
+        if hit then
+            if is_head_in_zone then
+                return segment.head:flat():distance(hit) / segment:lengthFlat()
+            else -- is_tail_in_zone then
+                return segment.tail:flat():distance(hit) / segment:lengthFlat()
+            end
+        end
+    end
+
+    Assert.Error([[
+        Calculated that either head or tail are in zone,
+        while the segment does not intersect with any of the zone's polygon sides]])
+end
+
 
 local function rotateVec2(v, theta)
     local new_x = v.x * math.cos(theta) - v.y * math.sin(theta)
@@ -230,6 +271,47 @@ end
 
 local Assert = require('drift-mode/assert')
 local function test()
+    -- Zone.isSegmentInZone
+    --   For debugging these it'd be best to draw a coordinate plane (x, z) and check
+    local inside = PointGroup.new{
+        Point.new("", vec3(0, 0, 0)),
+        Point.new("", vec3(1, 0, 0)) }
+    local outside = PointGroup.new{
+        Point.new("", vec3(0, 0, 1)),
+        Point.new("", vec3(1, 0, 1))}
+
+    local zone = Zone.new("test", outside, inside, 0)
+    local custom_origin = Point.new("", vec3(23.45, 0, 51.23))
+
+    local segment = Segment.new(
+        Point.new("", vec3(2, 0, 0)),
+        Point.new("", vec3(2, 0, 2)))
+    Assert.Equal(zone:isSegmentInZone(segment, custom_origin), 0.0, "Zone.isZoneInSegment() test failed")
+
+    segment = Segment.new(
+        Point.new("", vec3(0.1, 0, 0.1)),
+        Point.new("", vec3(0.5, 0, 0.8)))
+    Assert.Equal(zone:isSegmentInZone(segment, custom_origin), 1.0, "Zone.isZoneInSegment() test failed")
+
+    segment = Segment.new(
+        Point.new("", vec3(0.5, 0, 0.5)),
+        Point.new("", vec3(0.5, 0, 2.5)))
+    Assert.Equal(zone:isSegmentInZone(segment, custom_origin), 0.25, "Zone.isZoneInSegment() test failed")
+
+    segment = Segment.new(
+        Point.new("", vec3(0.5, 0, 0.5)),
+        Point.new("", vec3(0.5, 0, 4.5)))
+    Assert.Equal(zone:isSegmentInZone(segment, custom_origin), 0.125, "Zone.isZoneInSegment() test failed")
+
+    segment = Segment.new(
+        Point.new("", vec3(0.5, 0, 4.5)),
+        Point.new("", vec3(0.5, 0, 0.5)))
+    Assert.Equal(zone:isSegmentInZone(segment, custom_origin), 0.125, "Zone.isZoneInSegment() test failed")
+
+    segment = Segment.new(
+        Point.new("", vec3(0.5, 0, 2.5)),
+        Point.new("", vec3(0.5, 0, 0.5)))
+    Assert.Equal(zone:isSegmentInZone(segment, custom_origin), 0.25, "Zone.isZoneInSegment() test failed")
 end
 test()
 
