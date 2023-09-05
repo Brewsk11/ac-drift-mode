9538ce07813e1f6e84dbdd23e1d0e57db2226442
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-09-05 23:17:09 +0200

    Add some color to resources

--- lua_libs/drift-mode/Resources.lua
+++ lua_libs/drift-mode/Resources.lua
@@ -6,6 +6,13 @@ local Resources = {
     IconZoneBlack = "D:\\Projects\\drift-mode\\res\\zone_icon_black.png",
     IconClipWhite = "D:\\Projects\\drift-mode\\res\\clip_icon_white.png",
     IconClipBlack = "D:\\Projects\\drift-mode\\res\\clip_icon_black.png",
+    LogoWhite = "D:\\Projects\\drift-mode\\res\\logo white.png",
+    ColorSpeed = rgbm(230 / 255, 138 / 255, 46 / 255, 1),
+    ColorAngle = rgbm(20 / 255, 204 / 255, 112 / 255, 1),
+    ColorDepth = rgbm(112 / 255, 20 / 255, 204 / 255, 1),
+    ColorNeutralSpeed = rgbm(148 / 255, 132 / 255, 122 / 255, 1),
+    ColorNeutralAngle = rgbm(120 / 255, 142 / 255, 125 / 255, 1),
+    ColorNeutralRatio = rgbm(102 / 255, 82 / 255, 106 / 255, 1),
 }
 
 
