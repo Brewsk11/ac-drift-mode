86ca5c460af4ce2341da41a98283fdaf8866437b
Paweł Szymichowski <p.szymichowski@protonmail.com>
2023-09-05 19:06:19 +0200

    Updated graphics and `Resources` helper

---
+++ lua_libs/drift-mode/Resources.lua
@@ -0,0 +1,12 @@
+local Assert = require('drift-mode/assert')
+
+---@enum Resources
+local Resources = {
+    IconZoneWhite = "D:\\Projects\\drift-mode\\res\\zone_icon_white.png",
+    IconZoneBlack = "D:\\Projects\\drift-mode\\res\\zone_icon_black.png",
+    IconClipWhite = "D:\\Projects\\drift-mode\\res\\clip_icon_white.png",
+    IconClipBlack = "D:\\Projects\\drift-mode\\res\\clip_icon_black.png",
+}
+
+
+return Resources
