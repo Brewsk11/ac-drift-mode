5c8a2b10dea87c3fd18d08ce5773de245358f261
Paweł Szymichowski <p.szymichowski@protonmail.com>
2025-02-22 09:56:40 +0100

    Add aggregation `ScoringObjectState` class methods

--- lua_libs/drift-mode/models/ScoringObjectState.lua
+++ lua_libs/drift-mode/models/ScoringObjectState.lua
@@ -67,6 +67,46 @@ function ScoringObjectState:drawFlat(coord_transformer, scale)
     Assert.Error("Called abstract method!")
 end
 
+---Given scoring objects array return the score of all objects combined
+---@param scoring_object_states ScoringObjectState[]
+---@return number
+function ScoringObjectState.aggrScore(scoring_object_states)
+    local score = 0
+    for _, scoring_object in ipairs(scoring_object_states) do
+        score = score + scoring_object:getScore()
+    end
+    return score
+end
+
+---Given scoring objects array return the max score of all objects combined
+---@param scoring_object_states ScoringObjectState[]
+---@return number
+function ScoringObjectState.aggrMaxScore(scoring_object_states)
+    local score = 0
+    for _, scoring_object in ipairs(scoring_object_states) do
+        score = score + scoring_object:getMaxScore()
+    end
+    return score
+end
+
+---Given scoring objects array return average score to max score ratio
+---only for scoring objects that are done
+---@param scoring_object_states ScoringObjectState[]
+---@return number
+function ScoringObjectState.aggrAvgScore(scoring_object_states)
+    local mult = 0
+    local scoring_finished = 0
+    for _, state in ipairs(scoring_object_states) do
+        if state:isDone() and state:getMultiplier() ~= nil then
+            mult = mult + state:getMultiplier()
+            scoring_finished = scoring_finished + 1
+        end
+    end
+    if scoring_finished == 0 then return 0 end
+    mult = mult / scoring_finished
+    return mult
+end
+
 local function test()
 end
 test()
