5efa9b99eafe9d4ed7d1f66a8b22bce94f611403
Paweł Szymichowski <p.szymichowski@protonmail.com>
2025-10-01 21:33:54 +0200

    Model tree refactor design files

---
+++ _prototype.lua
@@ -0,0 +1,20 @@
+local Zone = class("Zone")
+local Common = require("drift-mode.models.Common")
+
+
+Zone.Model = class("Zone.Model")
+Zone.Model.__model_path = "TrackObjects.Zone:Model"
+
+Zone.State = class("Zone.State")
+Zone.Model.__model_path = "TrackObjects.Zone:State"
+
+Zone.Drawers = {}
+
+Zone.Drawers.Base = class("Zone:Drawers:Base", Common.DrawerBase)
+Zone.Drawers.Base.__model_path = "TrackObjects.Zone:State"
+
+Zone.Drawers.Run = class("Zone:Drawers:Run", Zone.Drawers.Base)
+Zone.Drawers.Run.__model_path = "TrackObjects.Zone:Drawers:Base"
+
+Zone.Drawers.Setup = class("Zone:Drawers:Setup", Zone.Drawers.Base)
+Zone.Drawers.Setup.__model_path = "TrackObjects.Zone:Drawers:Setup"
