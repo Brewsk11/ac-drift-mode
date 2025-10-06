fa4ea24c2e9603e65e414f4083652236eee9bf45
Paweł Szymichowski <p.szymichowski@protonmail.com>
2025-10-06 22:24:21 +0200

    Removed unused method

--- lua_libs/drift-mode/models/Elements/Position/StartingPoint.lua
+++ lua_libs/drift-mode/models/Elements/Position/StartingPoint.lua
@@ -18,13 +18,6 @@ function StartingPoint:setEnd(new_end_point)
     self.direction = (new_end_point:value() - self.origin:value()):normalize()
 end
 
-function StartingPoint:drawSetup()
-    self.origin:draw(0.6)
-    render.debugArrow(self.origin:value(), self.origin:value() + self.direction, 0.1)
-    render.debugLine(self.origin:value(), self.origin:value() + vec3(0, 2, 0))
-    render.debugText(self.origin:value() + vec3(0, 2, 0), "Starting point")
-end
-
 function StartingPoint:drawFlat(coord_transformer, scale, color)
     -- TODO: Dep injection
     ui.drawCircleFilled(coord_transformer(self.origin), scale * 1, color)
