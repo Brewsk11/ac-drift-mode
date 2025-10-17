4d136216d159893da963760c2d3caad367709b7a
Paweł Szymichowski <p.szymichowski@protonmail.com>
2025-10-17 14:05:17 +0200

    Hotfix for `PositionHandle`

--- lua_libs/drift-mode/models/Elements/Position/Handle.lua
+++ lua_libs/drift-mode/models/Elements/Position/Handle.lua
@@ -2,29 +2,29 @@ local Handle = require("drift-mode.models.Elements.Handle")
 local Point = require("drift-mode.models.Common.Point.Point")
 
 
----@class PoisitionHandle : Handle
-local PoisitionHandle = class("PoisitionHandle", Handle)
-PoisitionHandle.__model_path = "Elements.Position.Handle"
+---@class PositionHandle : Handle
+local PositionHandle = class("PoisitionHandle", Handle)
+PositionHandle.__model_path = "Elements.Position.Handle"
 
----@enum PoisitionHandle.Type
-PoisitionHandle.Type = {
+---@enum PositionHandle.Type
+PositionHandle.Type = {
   Origin = "Origin",
   Ending = "Ending"
 }
 
-function PoisitionHandle:initialize(point, position, type)
+function PositionHandle:initialize(point, position, type)
   Handle.initialize(self, point, position)
   self.type = type
 end
 
-function PoisitionHandle:set(new_pos)
+function PositionHandle:set(new_pos)
   local position = self.element
   ---@cast position Position
-  if self.type == PoisitionHandle.Type.Origin then
-    self.position.origin:set(new_pos)
-  elseif self.type == PoisitionHandle.Type.Ending then
-    self.position:setEnd(Point(new_pos))
+  if self.type == PositionHandle.Type.Origin then
+    position.origin:set(new_pos)
+  elseif self.type == PositionHandle.Type.Ending then
+    position:setEnd(Point(new_pos))
   end
 end
 
-return PoisitionHandle
+return PositionHandle
