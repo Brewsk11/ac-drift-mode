73352d7749ecfc0ddc686b28bf2338245cae6bd5
Paweł Szymichowski <p.szymichowski@protonmail.com>
2025-10-27 22:07:02 +0100

    Add an option to segment's `Wall` drawer to align to track

--- source/common/drift-mode/models/Common/Segment/Drawers/Wall.lua
+++ source/common/drift-mode/models/Common/Segment/Drawers/Wall.lua
@@ -1,4 +1,6 @@
 local DrawerSegment = require("drift-mode.models.Common.Segment.Drawers.Base")
+local RaycastUtils = require("drift-mode.RaycastUtils")
+
 
 ---@class DrawerSegmentWall : DrawerSegment
 ---@field color rgbm
@@ -9,15 +11,36 @@ DrawerSegmentWall.__model_path = "Common.Segment.Drawers.Wall"
 function DrawerSegmentWall:initialize(color, height)
     self.color = color or rgbm(1, 1, 1, 1)
     self.height = height or 1
+    self.aligned = true
+
+    self:cacheMethod("getQuadCoords")
+end
+
+function DrawerSegmentWall:getQuadCoords(segment, aligned)
+    if aligned then
+        return {
+            RaycastUtils.getAlignedToTrack(segment:getHead():value()),
+            RaycastUtils.getAlignedToTrack(segment:getTail():value()),
+            RaycastUtils.getAlignedToTrack(segment:getTail():value()) + vec3(0, self.height, 0),
+            RaycastUtils.getAlignedToTrack(segment:getHead():value()) + vec3(0, self.height, 0)
+        }
+    else
+        return {
+            segment:getHead():value(),
+            segment:getTail():value(),
+            segment:getTail():value() + vec3(0, self.height, 0),
+            segment:getHead():value() + vec3(0, self.height, 0)
+        }
+    end
 end
 
 ---@param segment Segment
 function DrawerSegmentWall.draw(self, segment)
     render.quad(
-        segment:getHead():value(),
-        segment:getTail():value(),
-        segment:getTail():value() + vec3(0, self.height, 0),
-        segment:getHead():value() + vec3(0, self.height, 0),
+        self:getQuadCoords(segment, self.aligned)[1],
+        self:getQuadCoords(segment, self.aligned)[2],
+        self:getQuadCoords(segment, self.aligned)[3],
+        self:getQuadCoords(segment, self.aligned)[4],
         self.color
     )
 end
