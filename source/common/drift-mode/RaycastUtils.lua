d8c5e1dde1abd6eeebf6bf5ddf5b73856cb50cc9
Paweł Szymichowski <p.szymichowski@protonmail.com>
2025-10-27 22:06:17 +0100

    Non-mutable `RaycastUtils` function

--- source/common/drift-mode/RaycastUtils.lua
+++ source/common/drift-mode/RaycastUtils.lua
@@ -1,3 +1,6 @@
+local Point = require("drift-mode.models.Common.Point.Point")
+
+
 local RaycastHelper = {}
 
 function RaycastHelper.getTrackRayMouseHit()
@@ -14,16 +17,30 @@ end
 ---@param point Point
 ---@return vec3?
 function RaycastHelper.alignPointToTrack(point)
-    -- Safeguard when calling from unsupported modules (app vs. mode etc.)
+    local aligned = RaycastHelper.getAlignedToTrack(point)
+    if aligned then
+        point:set(aligned)
+    end
+end
+
+---@param point vec3|Point
+---@return vec3?
+function RaycastHelper.getAlignedToTrack(point)
     if not physics then
         ac.warn("Could not call RaycastHelper.alignPointToTrack() due to not available `physics`")
         return
     end
 
+    local vec
+    if Point.isInstanceOf(point) then
+        vec = point:value()
+    else
+        vec = point
+    end
+
     local hit = vec3()
-    local dist = physics.raycastTrack(point:value() + vec3(0, 30, 0), vec3(0, -1, 0), 50, hit)
+    local dist = physics.raycastTrack(vec + vec3(0, 30, 0), vec3(0, -1, 0), 50, hit)
     if dist ~= -1 then
-        point:set(hit)
         return hit
     end
 end
